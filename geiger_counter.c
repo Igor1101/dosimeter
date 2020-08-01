6f07b2a38a3cd722025da5baea0e288c3018ea6b
Igor1101 <igor.muravyov.2020@ukr.net>
2020-08-01 23:13:35 +0300

    added sources

---
+++ geiger_counter.c
@@ -0,0 +1,42 @@
+/* geiger_counter.c
+ * tube driver code
+ */
+#include "serial.h"
+#include "geiger_counter.h"
+// for M4011
+#define CPM 151
+// for SI-22g
+// #define CPM
+// for 1 mk
+// for
+// counting number
+static uint32_t counter=0;
+// time period to measure
+const uint32_t geiger_counter_period_ms = 60000;
+
+// interrupt 0 on rising edge
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(GPIO_Pin);
+  /* NOTE: This function Should not be modified, when the callback is needed,
+           the HAL_GPIO_EXTI_Callback could be implemented in the user file
+   */
+	counter++;
+}
+
+/* tim_geiger_counter_callback function */
+void tim_geiger_counter_callback(void const * argument)
+{
+	pr_debugln("Counter=%d counts, %d MkRoentgen %d NanoSv", counter,
+			(counter*100)/CPM,
+			(counter*1000)/CPM);
+	counter = 0;
+}
+
+void geiger_counter_init(void)
+{
+	pr_debugln("starting geiger counter");
+	osTimerStart(tim_geiger_counterHandle, geiger_counter_period_ms);
+}
---
+++ geiger_counter.h
@@ -0,0 +1,17 @@
+/*
+ * geiger_counter.h
+ * geiger tube driver
+ */
+
+#ifndef INC_GEIGER_COUNTER_H_
+#define INC_GEIGER_COUNTER_H_
+
+#include "cmsis_os.h"
+
+extern const uint32_t geiger_counter_period_ms;
+extern osTimerId tim_geiger_counterHandle;
+
+
+void geiger_counter_init(void);
+
+#endif /* INC_GEIGER_COUNTER_H_ */
