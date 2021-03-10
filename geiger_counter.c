2c5b8d83d8cc23528ec8fa9915b65b26d53149dc
Igor1101 <igor.muravyov.2020@ukr.net>
2021-03-10 17:35:30 +0200

    add last value memory

--- geiger_counter.c
+++ geiger_counter.c
@@ -3,6 +3,7 @@
  */
 #include <generic/serial.h>
 #include "geiger_counter.h"
+#include <generic/display.h>
 // for M4011
 #define CPM 151
 // for SI-22g
@@ -10,34 +11,32 @@
 // for 1 mk
 // for
 // counting number
-static uint32_t counter=0;
+static volatile uint32_t counter=0;
 // time period to measure
 const uint32_t geiger_counter_period_ms = 60000;
 
+uint32_t geiger_counter_mkroentgen_last = 0;
+uint32_t geiger_counter_nanosv_last = 0;
 // interrupt 0 on rising edge
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+void geiger_counter_int_callback(void)
 {
-  /* Prevent unused argument(s) compilation warning */
-  UNUSED(GPIO_Pin);
-  /* NOTE: This function Should not be modified, when the callback is needed,
-           the HAL_GPIO_EXTI_Callback could be implemented in the user file
-   */
 	counter++;
 }
 
 /* tim_geiger_counter_callback function
  * this should be started each 1 minute */
-void tim_geiger_counter_callback_1m(void const * argument)
+void geiger_counter_callback(uint32_t time)
 {
-	pr_debugln("Counter=%d counts, %d MkRoentgen %d NanoSv", counter,
-			(counter*100)/CPM,
-			(counter*1000)/CPM);
+	geiger_counter_mkroentgen_last = (counter*100 * time)/CPM / 60;
+	geiger_counter_nanosv_last = (counter*1000 * time)/CPM / 60;
+	tty_println("Counter=%d counts, %d MkRoentgen %d NanoSv", counter,
+			geiger_counter_mkroentgen_last,
+			geiger_counter_nanosv_last);
 	counter = 0;
 }
 
 void geiger_counter_init(void)
 {
-	pr_debugln("starting geiger counter");
-	//osTimerStart(tim_geiger_counterHandle, geiger_counter_period_ms);
+	tty_println("starting geiger counter");
 }
--- geiger_counter.h
+++ geiger_counter.h
@@ -13,5 +13,9 @@ extern const uint32_t geiger_counter_period_ms;
 
 void geiger_counter_init(void);
 void tim_geiger_counter_callback_1m(void const * argument);
+void geiger_counter_int_callback(void);
+
+extern uint32_t geiger_counter_mkroentgen_last;
+extern uint32_t geiger_counter_nanosv_last;
 
 #endif /* INC_GEIGER_COUNTER_H_ */
